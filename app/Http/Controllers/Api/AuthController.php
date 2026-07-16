de9dc22500cbdb02dafa7fd7a2644a7cf6c863fb
Calebe Copello <calebecopello@gmail.com>
2026-07-16 00:01:47 -0300

    chore: AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,62 +3,58 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required', 'string']
         ]);
 
-        if (!$token = JWTAuth::attempt($credentials)) {
-            throw ValidationException::withMessages([
-                'email' => __('auth.failed'),
-            ]);
+        $token = JWTAuth::attempt($credentials);
+
+        if (!$token) {
+            return $this->returnAuthPayload(message: 'The provided credentials are incorrect.', token: null, user: null, errors: ['email' => ['The provided credentials are incorrect.']], httpCode: 401);
         }
 
-        return response()->json($this->returnToken($token), 200);
+        return $this->returnAuthPayload(message: 'You are logged in.', token: $token, user: JWTAuth::user(), errors: null, httpCode: 200);
     }
 
-    public function refresh()
+    public function refresh(): JsonResponse
     {
         $newToken = JWTAuth::refresh();
 
-        return response()->json($this->returnToken($newToken), 200);
+        return $this->returnAuthPayload(message: 'Your token was refreshed.', token: $newToken, user: JWTAuth::setToken($newToken)->toUser(), errors: null, httpCode: 200);
     }
 
-    public function logout()
+    public function logout(): JsonResponse
     {
         JWTAuth::logout();
-        return response()->json(['message' => 'You logged out.'], 200);
+        return $this->returnAuthPayload(message: 'You logged out.', token: null, user: null, errors: null, httpCode: 200);
     }
 
-    public function me()
+    public function me(): JsonResponse
     {
-        $user = JWTAuth::user();
+        return $this->returnAuthPayload(message: 'Request succeeded.', token: null, user: JWTAuth::user(), errors: null, httpCode: 200);
+
+    }
+
+    private function returnAuthPayload(string $message, ?string $token, mixed $user, ?array $errors, int $httpCode): JsonResponse
+    {
         return response()->json([
-            'user' => $user,
-        ], 200);
-    }
-
-    private function returnToken(string $token): array
-    {
-        return [
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => JWTAuth::factory()->getTTL() * 60,
-            'user' => JWTAuth::user(),
-            'message' => 'You are logged in.'
-        ];
-    }
-
-    //TODO: json standard format to frontend
-    private function returnAuthPayload() {
-        return;
+            'message' => $message,
+            'data' => [
+                'access_token' => $token,
+                'token_type' => $token !== null ? 'bearer' : null,
+                'expires_in' => $token !== null ? JWTAuth::factory()->getTTL() * 60 : null,
+                'user' => $user,
+            ],
+            'errors' => $errors,
+        ], $httpCode);
     }
 }
