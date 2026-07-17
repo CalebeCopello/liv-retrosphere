de41a5d7bc63dadaf907c3799697d2ce5bac058c
Calebe Copello <calebecopello@gmail.com>
2026-07-16 23:45:47 -0300

    chore: fix routes and controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
+    public function register(Request $request): JsonResponse {
+        $payload = $request->validate([
+            'username' => ['required', 'string', 'unique:users,username'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required','string', 'min:4', 'max:32']
+        ]);
+
+        $user = User::create([
+            'username' => $payload['username'],
+            'display_name' => $payload['username'] . bin2hex(random_bytes(2)),
+            'email' => $payload['email'],
+            'password' => $payload['password'],
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return $this->returnAuthPayload(message: 'Your account was created.', token: $token, user: $user, errors: null, httpCode: 201);
+    }
     public function login(Request $request): JsonResponse
     {
         $credentials = $request->validate([
@@ -27,14 +47,14 @@ class AuthController extends Controller
 
     public function refresh(): JsonResponse
     {
-        $newToken = JWTAuth::refresh();
+        $newToken = JWTAuth::parseToken()->refresh();
 
         return $this->returnAuthPayload(message: 'Your token was refreshed.', token: $newToken, user: JWTAuth::setToken($newToken)->toUser(), errors: null, httpCode: 200);
     }
 
     public function logout(): JsonResponse
     {
-        JWTAuth::logout();
+        JWTAuth::parseToken()->invalidate();
         return $this->returnAuthPayload(message: 'You logged out.', token: null, user: null, errors: null, httpCode: 200);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::post('/auth/login',[App\Http\Controllers\Api\AuthController::class, 'login'])->name('auth.login');
+Route::prefix('auth')->group(function () {
+    Route::post('/login',[App\Http\Controllers\Api\AuthController::class, 'login'])->name('auth.login');
+    Route::post('/register',[App\Http\Controllers\Api\AuthController::class, 'register'])->name('auth.register');
+    Route::post('/refresh', [App\Http\Controllers\Api\AuthController::class, 'refresh'])->name('auth.refresh');
+});
 
 Route::middleware('auth:api')->prefix('auth')->group(function () {
     Route::post('logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->name('auth.logout');
-    Route::post('refresh', [App\Http\Controllers\Api\AuthController::class, 'refresh'])->name('auth.refresh');
 });
 
 Route::middleware('auth:api')->get('/me', [App\Http\Controllers\Api\AuthController::class, 'me'])->name('auth.me');
