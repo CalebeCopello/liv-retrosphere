1ddd7356887885142e8a38572e3cc764c982e68a
Calebe Copello <calebecopello@gmail.com>
2026-07-17 00:07:19 -0300

    chore: fix api routes

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login',[App\Http\Controllers\Api\AuthController::class, 'login'])->name('auth.login');
-    Route::post('/register',[App\Http\Controllers\Api\AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login'])->name('auth.login');
+    Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register'])->name('auth.register');
     Route::post('/refresh', [App\Http\Controllers\Api\AuthController::class, 'refresh'])->name('auth.refresh');
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->name('auth.logout');
+        Route::get('/me', [App\Http\Controllers\Api\AuthController::class, 'me'])->name('auth.me');
+    });
 });
-
-Route::middleware('auth:api')->prefix('auth')->group(function () {
-    Route::post('logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->name('auth.logout');
-});
-
-Route::middleware('auth:api')->get('/me', [App\Http\Controllers\Api\AuthController::class, 'me'])->name('auth.me');
