99000aee0ef94b0fb6f774baaadbb0032110f06f
Calebe Copello <calebecopello@gmail.com>
2026-07-18 02:35:11 -0300

    chore: fix duplicate /me route

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ Route::prefix('auth')->group(function () {
     Route::post('/refresh', [App\Http\Controllers\Api\AuthController::class, 'refresh'])->name('auth.refresh');
     Route::middleware('auth:api')->group(function () {
         Route::post('logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->name('auth.logout');
-        Route::get('/me', [App\Http\Controllers\Api\AuthController::class, 'me'])->name('auth.me');
     });
 });
 
