2e70124a25da6d6fd563cff93ee2a55e26213484
Calebe Copello <calebecopello@gmail.com>
2026-07-31 22:51:49 -0300

    chore: added tsconfig and fix vue+ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,14 +1,26 @@
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import {
+    createApp,
+    h,
+    type DefineComponent,
+} from 'vue';
+
+const appName =
+    import.meta.env.VITE_APP_NAME ?? 'LIV RetroSphere';
 
 createInertiaApp({
+    title: (title) =>
+        title ? `${title} - ${appName}` : appName,
+
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            import.meta.glob<DefineComponent>(
+                './Pages/**/*.vue',
+            ),
         ),
 
     setup({ el, App, props, plugin }) {
@@ -17,5 +29,9 @@ createInertiaApp({
         })
             .use(plugin)
             .mount(el);
+    },
+
+    progress: {
+        color: '#8cff98',
     },
 });
