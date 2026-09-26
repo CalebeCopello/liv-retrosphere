80e3402206051936e111ffa6cf653904863dc106
Calebe Copello <calebecopello@gmail.com>
2026-09-26 00:02:56 -0300

    chore: add relation to Model

--- app/Models/Platform.php
+++ app/Models/Platform.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Platform extends Model
@@ -31,5 +32,13 @@ class Platform extends Model
             'initial_release_year' => 'integer',
             'is_shown' => 'boolean',
         ];
+    }
+
+    public function manufacturers(): BelongsToMany
+    {
+        return $this->belongsToMany(Manufacturer::class, 'platform_manufacturer', 'platform_id', 'manufacturer_id')
+            ->using(PlatformManufacturer::class)
+            ->withPivot('id')
+            ->withTimestamps();
     }
 }
